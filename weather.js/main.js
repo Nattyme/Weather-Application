620c7826c6dff253cc9fac54f5c04fe506ee2d2c
NeuralNat <ifrance@yandex.ru>
2023-11-17 17:52:49 +0300

    error check, error card display

--- weather.js/main.js
+++ weather.js/main.js
@@ -1,41 +1,47 @@
-const apiKey = '455c33619df04d9cb2a94255231711';
+const apiKey = "455c33619df04d9cb2a94255231711";
 
-// Элементы на странице 
-const header = document.querySelector('.header');
-const form = document.querySelector('#form');
-const input = document.querySelector('#inputCity');
+// Элементы на странице
+const header = document.querySelector(".header");
+const form = document.querySelector("#form");
+const input = document.querySelector("#inputCity");
 
-// Слушаем отправку формы 
-form.onsubmit = function (e){
-    //Отменяем отправку формы
-    e.preventDefault();
+// Слушаем отправку формы
+form.onsubmit = function (e) {
+  //Отменяем отправку формы
+  e.preventDefault();
 
-    // БЕрем значение из input, обрезаем пробелы 
-    let city = input.value.trim();
-    
-    
-    // Адрес запроса
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  // БЕрем значение из input, обрезаем пробелы
+  let city = input.value.trim();
 
-    // Выполняем запрос погоды
-    fetch(url).then((response) => {
-        return response.json()
+  // Адрес запроса
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  // Выполняем запрос погоды
+  fetch(url)
+    .then((response) => {
+      return response.json();
     })
     .then((data) => {
-        console.log(data);
-        console.log(data.location.name);
-        console.log(data.location.country);
-        console.log(data.current.temp_c);
-        console.log(data.current.condition.text);
+      console.log(data);
+      // Проверка на ошибку
+      if (data.error) {
+        // Если есть ошибка - выводим ее
 
+        // Удаляем предыдущую карточку
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
+
+        const html = `<div class="card">${data.error.message}</div>`;
+        // Отображаем карточку на странице
+        header.insertAdjacentHTML("afterend", html);
+      } else {
+        // Если ошибки нет - выводим карточку
         // Отображаем полученные данные в карточке
+        // Удаляем предыдущую карточку
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
 
-        // Удаляем предыдущую карточку 
-        const prevCard = document.querySelector('.card');
-        if(prevCard) prevCard.remove();
-
-
-        // Разметка для карточки 
+        // Разметка для карточки
         const html = `
         <div class="card">
         <h2 class="card__city">${data.location.name}<span>${data.location.country}</span></h2>
@@ -48,7 +54,8 @@ form.onsubmit = function (e){
       </div>
       `;
 
-      // Отображаем карточку на странице 
-      header.insertAdjacentHTML('afterend', html);
+        // Отображаем карточку на странице
+        header.insertAdjacentHTML("afterend", html);
+      }
     });
-}
+};
